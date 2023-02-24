8861eb772017e38408e10dcd5bbb47f1f58a0dd6
Marijan Dupkovic <mdu@dupko-it.de>
2023-02-24 23:10:19 +0100

    Filled with 10 Questions per category

--- vars.js
+++ vars.js
@@ -106,6 +106,258 @@ let questions = [{
     'answer3': '<input type="email">',
     'answer4': '<input type="url">',
     'correctAnswer': 1
+},
+{
+    'category': 'css',
+    'question': 'Was ist die Bedeutung von "CSS"?',
+    'answer1': 'Computer Style Sheets',
+    'answer2': 'Cascading Style Sheets',
+    'answer3': 'Creative Style Sheets',
+    'answer4': 'Content Style Sheets',
+    'correctAnswer': 2
+},
+{
+    'category': 'css',
+    'question': 'Welches CSS-Attribut wird verwendet, um den Hintergrund eines Elements festzulegen?',
+    'answer1': 'background-color',
+    'answer2': 'text-color',
+    'answer3': 'font-style',
+    'answer4': 'border-style',
+    'correctAnswer': 1
+},
+{
+    'category': 'css',
+    'question': 'Welches CSS-Attribut wird verwendet, um den Abstand zwischen den Elementen zu definieren?',
+    'answer1': 'padding',
+    'answer2': 'margin',
+    'answer3': 'border',
+    'answer4': 'float',
+    'correctAnswer': 2
+},
+{
+    'category': 'css',
+    'question': 'Welches CSS-Attribut wird verwendet, um die Schriftgröße eines Elements zu definieren?',
+    'answer1': 'text-size',
+    'answer2': 'font-style',
+    'answer3': 'font-size',
+    'answer4': 'text-style',
+    'correctAnswer': 3
+},
+{
+    'category': 'css',
+    'question': 'Welches CSS-Attribut wird verwendet, um einen Text fett zu machen?',
+    'answer1': 'text-style',
+    'answer2': 'font-style',
+    'answer3': 'text-weight',
+    'answer4': 'font-weight',
+    'correctAnswer': 4
+},
+{
+    'category': 'css',
+    'question': 'Welches CSS-Attribut wird verwendet, um den Text in einem Element zu zentrieren?',
+    'answer1': 'text-align: center;',
+    'answer2': 'text-align: left;',
+    'answer3': 'text-align: right;',
+    'answer4': 'text-align: justify;',
+    'correctAnswer': 1
+},
+{
+    'category': 'js',
+    'question': 'Was ist JavaScript?',
+    'answer1': 'Eine Skriptsprache, die hauptsächlich für die Entwicklung von Webanwendungen verwendet wird.',
+    'answer2': 'Eine Programmiersprache, die nur für die Entwicklung von Desktop-Anwendungen verwendet wird.',
+    'answer3': 'Eine Datenbank-Management-Sprache für die Verwaltung von relationalen Datenbanken.',
+    'answer4': 'Eine Markup-Sprache für die Erstellung von Webseiten.',
+    'correctAnswer': 1
+},
+{
+    'category': 'js',
+    'question': 'Welches Schlüsselwort wird verwendet, um eine Funktion in JavaScript zu deklarieren?',
+    'answer1': 'func',
+    'answer2': 'fun',
+    'answer3': 'def',
+    'answer4': 'function',
+    'correctAnswer': 4
+},
+{
+    'category': 'js',
+    'question': 'Welche Methode wird verwendet, um ein Element über die "id" in JavaScript zu finden?',
+    'answer1': 'getElementByClass',
+    'answer2': 'getElementById',
+    'answer3': 'getElementByTagName',
+    'answer4': 'getElementByName',
+    'correctAnswer': 2
+},
+{
+    'category': 'js',
+    'question': 'Welche Methode wird verwendet, um eine Zeichenkette in JavaScript zu teilen?',
+    'answer1': 'splice()',
+    'answer2': 'slice()',
+    'answer3': 'split()',
+    'answer4': 'splitstring()',
+    'correctAnswer': 3
+},
+{
+    'category': 'js',
+    'question': 'Welche Methode wird verwendet, um eine Zeichenkette in Kleinbuchstaben zu konvertieren?',
+    'answer1': 'toLowerCase()',
+    'answer2': 'toUpperCase()',
+    'answer3': 'convertToLower()',
+    'answer4': 'convertToUpperCase()',
+    'correctAnswer': 1
+},
+{
+    'category': 'js',
+    'question': 'Was ist der Zweck der Schlüsselwörter break und continue in JavaScript?',
+    'answer1': 'break wird verwendet, um eine Funktion zu beenden, während continue die Schleife überspringt und mit der nächsten Iteration fortfährt.',
+    'answer2': 'break wird verwendet, um eine Schleife zu beenden, während continue die Schleife überspringt und mit der nächsten Iteration fortfährt.',
+    'answer3': 'break und continue sind synonyme Schlüsselwörter und können austauschbar verwendet werden.',
+    'answer4': ' break und continue haben keinen Zweck in JavaScript.',
+    'correctAnswer': 2
+},
+{
+    'category': 'js',
+    'question': 'Was ist der Zweck der Funktion setTimeOut() in JavaScript?',
+    'answer1': 'Es ruft eine Funktion zu einem bestimmten Zeitpunkt auf.',
+    'answer2': 'Es überprüft, ob eine Bedingung wahr ist, bevor eine Funktion ausgeführt wird.',
+    'answer3': 'Es ruft eine Funktion in regelmäßigen Abständen auf.',
+    'answer4': 'Es verzögert die Ausführung einer Funktion um eine bestimmte Anzahl von Millisekunden.',
+    'correctAnswer': 4
+},
+{
+    'category': 'js',
+    'question': 'Was ist der Unterschied zwischen let und const in JavaScript?',
+    'answer1': 'let definiert eine lokale Variable, während const eine globale Variable definiert.',
+    'answer2': 'let kann nur einmal definiert werden, während const mehrfach definiert werden kann.',
+    'answer3': 'let kann nur in Schleifen verwendet werden, während const in jeder Funktion ',
+    'answer4': 'let ermöglicht das Ändern des Werts einer Variablen, während const es nicht zulässt.',
+    'correctAnswer': 4
+},
+{
+    'category': 'js',
+    'question': 'Was ist der Unterschied zwischen null und undefined in JavaScript?',
+    'answer1': 'Null ist ein vordefinierter Wert in JavaScript, während Undefined bedeutet, dass eine Variable nicht initialisiert wurde.',
+    'answer2': 'Null bedeutet, dass eine Variable keinen Wert hat, während Undefined bedeutet, dass eine Variable nicht vorhanden ist.',
+    'answer3': 'Null und Undefined sind Synonyme und können austauschbar verwendet werden.',
+    'answer4': 'Null gibt an, dass eine Variable unbenutzt ist, während Undefined eine ungültige Operation darstellt.',
+    'correctAnswer': 2
+},
+{
+    'category': 'js',
+    'question': 'Was ist der Zweck von JSON.parse() in JavaScript?',
+    'answer1': 'Es wird verwendet, um ein JavaScript-Objekt in eine JSON-Zeichenkette zu konvertieren.',
+    'answer2': 'Es wird verwendet, um eine Funktion aus einer JSON-Zeichenkette zu erstellen.',
+    'answer3': 'Es wird verwendet, um eine JSON-Zeichenkette in ein JavaScript-Objekt zu konvertieren.',
+    'answer4': ' Es wird verwendet, um eine Funktion in eine JSON-Zeichenkette zu konvertieren.',
+    'correctAnswer': 3
+},
+{
+    'category': 'java',
+    'question': 'Was ist der Unterschied zwischen einer Klasse und einem Objekt in Java?',
+    'answer1': 'Eine Klasse und ein Objekt sind Synonyme und können austauschbar verwendet werden.',
+    'answer2': 'Eine Klasse ist eine Vorlage für die Erstellung von Objekten, während ein Objekt eine Instanz einer Klasse ist.',
+    'answer3': 'Eine Klasse ist eine spezielle Art von Objekt, die von anderen Objekten abgeleitet werden kann.',
+    'answer4': 'Ein Objekt ist eine spezielle Art von Klasse, die von anderen Klassen abgeleitet werden kann.',
+    'correctAnswer': 2
+},
+{
+    'category': 'java',
+    'question': 'Was ist eine Ausnahme in Java?',
+    'answer1': 'Ein Fehler, der während der Kompilierung auftritt.',
+    'answer2': 'Ein Zustand, in dem der Speicher vollständig ausgelastet ist.',
+    'answer3': 'Eine Bedingung, die während der Ausführung des Programms auftritt und den normalen Ablauf unterbricht.',
+    'answer4': 'Ein Zustand, in dem die Netzwerkverbindung unterbrochen ist.',
+    'correctAnswer': 3
+},
+{
+    'category': 'java',
+    'question': 'Was ist der Zweck von try-catch-Blöcken in Java?',
+    'answer1': 'Sie dienen dazu, Ausnahmen abzufangen und darauf zu reagieren.',
+    'answer2': 'Sie dienen dazu, den Speicherbedarf des Programms zu optimieren.',
+    'answer3': 'Sie dienen dazu, die Ausführungsgeschwindigkeit des Programms zu erhöhen.',
+    'answer4': 'Sie dienen dazu, Variablen in einer definierten Reihenfolge zu initialisieren.',
+    'correctAnswer': 1
+},
+{
+    'category': 'java',
+    'question': 'Welches Schlüsselwort wird in Java verwendet, um eine Klasse zu definieren?',
+    'answer1': 'method',
+    'answer2': 'object',
+    'answer3': 'variable',
+    'answer4': 'class',
+    'correctAnswer': 4
+},
+{
+    'category': 'java',
+    'question': 'Welches Schlüsselwort wird in Java verwendet, um eine Methode zu definieren?',
+    'answer1': 'class',
+    'answer2': 'method',
+    'answer3': 'object',
+    'answer4': 'variable',
+    'correctAnswer': 2
+},
+{
+    'category': 'java',
+    'question': 'Was ist das Konzept von Vererbung in Java?',
+    'answer1': 'Es ermöglicht die Definition von Methoden mit demselben Namen in verschiedenen Klassen.',
+    'answer2': 'Es ermöglicht das Erben von Eigenschaften und Methoden von einer Schnittstelle in eine Klasse.',
+    'answer3': 'Es ermöglicht das Erben von Eigenschaften und Methoden von einer Klasse in eine andere Klasse.',
+    'answer4': 'Es ermöglicht das Erben von Eigenschaften und Methoden von einer Klasse in eine Schnittstelle.',
+    'correctAnswer': 3
+},
+{
+    'category': 'java',
+    'question': 'Welches Schlüsselwort wird in Java verwendet, um eine neue Instanz einer Klasse zu erstellen?',
+    'answer1': 'new',
+    'answer2': 'create',
+    'answer3': 'make',
+    'answer4': 'build',
+    'correctAnswer': 1
+},
+{
+    'category': 'java',
+    'question': 'Was ist eine Interface in Java?',
+    'answer1': 'Eine Klasse, die nicht direkt instanziiert werden kann.',
+    'answer2': 'Eine Möglichkeit, einen Wert zurückzugeben, wenn eine Methode kein Ergebnis hat.',
+    'answer3': 'Eine Möglichkeit, die gemeinsamen Eigenschaften von Klassen zu definieren.',
+    'answer4': 'Eine Möglichkeit, einen Standardwert für eine Variable festzulegen.',
+    'correctAnswer': 3
+},
+{
+    'category': 'java',
+    'question': 'Was ist der Unterschied zwischen einem privaten und einem öffentlichen Attribut in Java?',
+    'answer1': 'Ein privates Attribut kann nur innerhalb der Klasse verwendet werden, während ein öffentliches Attribut in allen Klassen verwendet werden kann.',
+    'answer2': 'Ein privates Attribut kann nicht geändert werden, während ein öffentliches Attribut geändert werden kann.',
+    'answer3': 'Ein privates Attribut kann nur einmal initialisiert werden, während ein öffentliches Attribut mehrmals initialisiert werden kann.',
+    'answer4': 'Ein privates Attribut kann von außerhalb der Klasse nicht zugegriffen werden, während ein öffentliches Attribut von überall zugegriffen werden kann.',
+    'correctAnswer': 4
+},
+{
+    'category': 'java',
+    'question': 'Was ist eine Enumeration in Java?',
+    'answer1': 'Eine Möglichkeit, eine abstrakte Klasse zu erstellen.',
+    'answer2': 'Eine Möglichkeit, eine feste Menge von Konstanten zu definieren.',
+    'answer3': 'Eine Möglichkeit, eine Klasse als Serialisierbar zu kennzeichnen.',
+    'answer4': 'Eine Möglichkeit, eine Klasse als final zu kennzeichnen.',
+    'correctAnswer': 2
+},
+{
+    'category': 'html',
+    'question': 'Was ist das Ziel des "title" Attributs im <img> Tag in HTML?',
+    'answer1': 'Es gibt den Dateipfad des Bildes an.',
+    'answer2': 'Es gibt die Breite und Höhe des Bildes an.',
+    'answer3': 'Es beschreibt den Inhalt des Bildes für barrierefreie Benutzer.',
+    'answer4': 'Es gibt den Titel des Bildes an.',
+    'correctAnswer': 4
+},
+{
+    'category': 'html',
+    'question': 'Welches HTML-Tag wird verwendet, um eine Liste mit Bulletpoints zu erstellen?',
+    'answer1': '<ul>',
+    'answer2': '<li>',
+    'answer3': '<ol>',
+    'answer4': '<nav>',
+    'correctAnswer': 1
 }
 ];
 
